f25fc181d23ffdf4300251702bd90c188a0a671e
Ryo Okuwaki <rokuwaki@gmail.com>
2021-08-13 00:23:15 +0100

    update script to check identity of EFS and original traces

--- Python/checktraces.py
+++ Python/checktraces.py
@@ -1,9 +1,11 @@
 '''
-A Python script to check if the data conversion (mseed <--> EFS) is OK
-usage: python checktraces.py
+A Python script to check if the data conversion (original <--> EFS) is OK
+usage: (do not forget quotation marks!)
+python checktraces.py '../EX_DATA/EFS_Example.efs' '../EX_DATA/CI/waveforms/*'
 '''
 
 import os
+import sys
 import numpy as np
 import obspy
 from EFSpy_module import EFS
@@ -11,25 +13,28 @@ import matplotlib.pyplot as plt
 
 def main():
 
+    # pass arguments
+    args = sys.argv
+    efsname = args[1]
+    original = args[2]
+
     # load EFS file
-    efsname = '../EX_DATA/EFS_Example.efs'
     efs_data = EFS(efsname)
 
-    # load original mseed files
-    miniSEED = '../EX_DATA/CI/waveforms/*'
-    st = obspy.read(miniSEED)
+    # load original original files
+    st = obspy.read(original)
 
-    # check if the data conversion between mseed and EFS is OK
+    # check if the data conversion between original and EFS is OK
     j = np.random.randint(0,len(st)) # random station ID
-    mseedTrace = st[j].copy()
+    originalTrace = st[j].copy()
     wf = efs_data.waveforms[j]
 
     # display station code, location, and channel
-    print('EFS  :', efs_data.waveforms[j]['stname'].strip(), efs_data.waveforms[j]['loccode'].strip(), efs_data.waveforms[j]['chnm'].strip())
-    print('mseed:', st[j].stats.station, st[j].stats.location, st[j].stats.channel)
+    print('EFS     :', efs_data.waveforms[j]['stname'].strip(), efs_data.waveforms[j]['loccode'].strip(), efs_data.waveforms[j]['chnm'].strip())
+    print('original:', st[j].stats.station, st[j].stats.location, st[j].stats.channel)
 
     # judge if the two traces are identical
-    if (wf['data'] == mseedTrace.data).all():
+    if (wf['data'] == originalTrace.data).all():
         print('two traces are identical!')
     else:
         print('something wrong...')
@@ -37,11 +42,11 @@ def main():
     # plot traces
     fig, (ax1, ax2) = plt.subplots(2, 1)
 
-    ax1.plot(wf['data'], lw=0.75, color='k', label='EFS: '+st[j].stats.station+' station')
-    ax1.plot(mseedTrace.data, lw=0.75, linestyle='-', color='r', label='mseed: '+efs_data.waveforms[j]['stname'].strip()+' station')
+    ax1.plot(wf['data'], color='k', label='EFS: '+st[j].stats.station+' station')
+    ax1.plot(originalTrace.data, color='r', label='original: '+efs_data.waveforms[j]['stname'].strip()+' station')
     ax1.legend()
 
-    ax2.plot(wf['data'] - mseedTrace.data, color='C7', label='diff')
+    ax2.plot(wf['data'] - originalTrace.data, color='C7', label='diff')
     ax2.legend()
     plt.show()
 
